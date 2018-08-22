54a6e6e76a0f8c2b3ca9d8ac799c344086551510
Alersandr Borkov <sbor767@yandex.ru>
2018-08-22 17:10:10 +0300

    Implemented input validation.

--- src/components/lists/tree-list/tree-list-item/index.js
+++ src/components/lists/tree-list/tree-list-item/index.js
@@ -6,6 +6,8 @@ import {themes} from '../../../../utils';
 import Input from '../../../elements/input'
 import Error from "../../../elements/error";
 
+const MAX_INPUT_LENGTH = 255;
+
 export default class TreeListItem extends Component {
 
   static propTypes = {
@@ -29,17 +31,22 @@ export default class TreeListItem extends Component {
 
   componentWillReceiveProps(nextProps) {
     const { id, title, isActive } = nextProps;
-    this.setState({ id, title, isActive });
+    let error = this.error(title);
+    this.setState({ id, title, isActive, error });
+    // this.setState({ id, title, isActive });
   }
 
+  error = (val) => !val.length || val.length > MAX_INPUT_LENGTH ? `Input length must be in 1..${MAX_INPUT_LENGTH} range!` : '';
+
   handleInputChange = val => {
-    if (val.length > 10) {
-      let err = new Error('')
-    }
-    this.setState({ title: val, isTouched: true });
+    let error = this.error(val);
+    console.log('handleInputChange-val', val, val.length, MAX_INPUT_LENGTH);
+    this.setState({ title: val, isTouched: true, error });
   };
 
   handleSubmit = () => {
+    console.log('handleSubmit_state', this.state);
+    if (this.state.error) return;
     this.props.onSubmit(this.state.id, this.state.title);
   };
 
@@ -59,22 +66,27 @@ export default class TreeListItem extends Component {
     const {theme, title, level, isActive } = this.props;
     return (
       <li
-        className={cn(`TreeListItem_${level}`, themes('TreeListItem', theme), isActive && 'TreeListItem_active')}
+        className={cn(
+          `TreeListItem_${level}`,
+          themes('TreeListItem', theme),
+          isActive && 'TreeListItem_active',
+        )}
         onClick={this.handleClick}
       >
         {!isActive ? (
           <div>{title}</div>
         ) : (
           <div>
-          <Input
-            type="text"
-            value={this.state.title}
-            onChange={this.handleInputChange}
-            focused={true}
-            tabIndex={0}
-            onBlur={this.handleSubmit}
-            onKeyDown={this.handleBodyKeyDown}
-          />
+            {this.state.error && <div className="TreeListItem__ErrorBox">Error {this.state.error}</div>}
+            <Input
+              type="text"
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              focused={true}
+              tabIndex={0}
+              onBlur={this.handleSubmit}
+              onKeyDown={this.handleBodyKeyDown}
+            />
           </div>
         )}
       </li>
